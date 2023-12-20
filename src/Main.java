10a4dd737cc5b9ab387714ff4263e07a88be86e5
Anna Korneevets <korneevecanna19@gmail.com>
2023-12-20 15:20:07 +0300

    second`

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.lang.Runnable;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String name = scanner.nextLine();
+        String str = "Hello " + name;
+        System.out.println(str);
+    }
+}
